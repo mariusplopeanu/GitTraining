c6173b504ad9018bd1661bdbdbb45542f439deea
Marius-Cristian Plopeanu <m.plopeanu@accenture.com>
2017-11-07 10:05:17 +0200

    Added Repository class in feature branch

---
+++ ConsoleApplicationRepository/ConsoleApplicationRepository/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationRepository
+{
+  class Repository
+  {
+    public string StringRepository { get; set; }
+  }
+}
