689866fa10228c510f9db43516ccad713169dee1
Marius-Cristian Plopeanu <m.plopeanu@accenture.com>
2017-11-07 10:02:08 +0200

    Creation of project

---
+++ ConsoleApplicationRepository/ConsoleApplicationRepository/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationRepository
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string str = "Hello world!";
+      Console.WriteLine(str);
+      Console.ReadKey();
+    }
+  }
+}
